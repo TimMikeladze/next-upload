ff5c1f5bbd0e75168b765a30ceb69e13bf0afb54
TimMikeladze <tim.mikeladze@gmail.com>
2023-08-03 01:08:12 -0700

    feat: add useNextUpload to example

--- examples/next-upload-example/src/components/FileUpload.tsx
+++ examples/next-upload-example/src/components/FileUpload.tsx
@@ -1,12 +1,14 @@
 'use client';
 
 import { useDropzone } from 'react-dropzone';
-import { upload } from 'next-upload/client';
+import { upload, useNextUpload } from 'next-upload/client';
 import { NextUploadType, config } from '@/app/upload/config';
 import toast from 'react-hot-toast';
 import bytes from 'bytes';
 
 const FileUpload = () => {
+  const nup = useNextUpload(config);
+
   const { getRootProps, getInputProps, isDragActive } = useDropzone({
     accept: {
       'image/*': ['.jpeg', '.png'],
@@ -16,15 +18,14 @@ const FileUpload = () => {
       toast.error(`Maximum file upload size is ${config.maxSize}`),
     onDropAccepted: (acceptedFiles) => {
       return toast.promise(
-        upload(
+        nup.upload(
           acceptedFiles.map((file) => ({
             file,
             uploadType: NextUploadType.image,
             metadata: {
               lastModified: file.lastModified,
             },
-          })),
-          config
+          }))
         ),
         {
           loading: 'Uploading...',
@@ -37,11 +38,39 @@ const FileUpload = () => {
 
   return (
     <div {...getRootProps()}>
-      <input {...getInputProps()} />
-      {isDragActive ? (
-        <p>Drop the files here ...</p>
-      ) : (
-        <p>Drag 'n' drop some files here, or click to select files</p>
+      <div
+        style={{
+          textAlign: 'center',
+        }}
+      >
+        <input {...getInputProps()} />
+        {isDragActive ? (
+          <p>Drop the files here ...</p>
+        ) : (
+          <p>Drag 'n' drop some files here, or click to select files</p>
+        )}
+      </div>
+      {nup.files.length > 0 && (
+        <div
+          style={{
+            display: 'flex',
+            flexWrap: 'wrap',
+            gap: '2rem',
+            marginTop: '2rem',
+          }}
+        >
+          {nup.files.map((x) => (
+            <img
+              key={x.webkitRelativePath}
+              src={URL.createObjectURL(x)}
+              style={{
+                objectFit: 'contain',
+                maxWidth: 300,
+                width: '100%',
+              }}
+            />
+          ))}
+        </div>
       )}
     </div>
   );
